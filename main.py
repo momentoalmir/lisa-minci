3cb26423a045f71eed58ebaf4a8b1ed81ee2d612
momentoalmir <momentoalmir@gmail.com>
2023-06-25 17:27:22 -0300

    Adicionar filtro de links fora do canal especificado

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 from models.weather import Weather
+from discord.utils import get
 
 from quotes.weather import messages as quotes_weather
 
@@ -13,6 +14,7 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_PREFIX = os.getenv('DISCORD_PREFIX', '!')
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+LINKS_CHANNEL_ID = os.getenv('LINKS_CHANNEL_ID')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -29,6 +31,21 @@ async def on_ready():
         type=discord.ActivityType.watching, name="Meu querido(a) ❤️"
     ))
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # Check if the message contains a link
+    # if not in links channel: delete message
+    links_channel = get(message.guild.channels, id=LINKS_CHANNEL_ID)
+
+    if 'http' in message.content:
+        if message.channel != links_channel:
+            await message.delete()
+
+        return await message.channel.send(f'{message.author.mention} Meu bem, é melhor você ter um cuidadinho especial com os links postados no nosso servidor do Discord, porque, você sabe, nem tudo que reluz é ouro na vastidão da internet, querido. ❤️')
+
 @bot.command()
 async def ping(ctx):
     # Calcula a latência do bot
@@ -43,6 +60,18 @@ async def clear(ctx, amount=5):
         return await ctx.send('Você não tem permissão para usar esse comando!')
 
     return await ctx.channel.purge(limit=amount)
+
+# Enviar mensagem ao usuário no canal do id do channel informado,
+# com os argumentos de id do channel e id do usuário e a mensagem no canal
+@bot.command()
+async def reply(ctx, channel_id, user_id, *, message):
+    if not ctx.author.guild_permissions.manage_messages:
+        return await ctx.send('Você não tem permissão para usar esse comando!')
+
+    channel = bot.get_channel(int(channel_id))
+    user = await bot.fetch_user(int(user_id))
+
+    await channel.send(f'{user.mention} {message}')
 
 @bot.command()
 async def clima(ctx, *, cidade=None):
